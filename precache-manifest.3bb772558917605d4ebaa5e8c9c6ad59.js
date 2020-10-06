5669797f52f911fca36a341141ea9e75d1a8e55e
Persovt <mza200339@gmail.com>
2020-10-06 19:57:24 +0300

    first commit

---
+++ precache-manifest.3bb772558917605d4ebaa5e8c9c6ad59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e1deb42580b9084b27a9a254c38b8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ed8918063520bc43100",
+    "url": "/static/css/main.cc0b6f0f.chunk.css"
+  },
+  {
+    "revision": "4df84d29cacf55fcaafd",
+    "url": "/static/js/2.74183e90.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.74183e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed8918063520bc43100",
+    "url": "/static/js/main.7dd50613.chunk.js"
+  },
+  {
+    "revision": "f89026c2cac52406a040",
+    "url": "/static/js/runtime-main.d6294cdf.js"
+  }
+]);
